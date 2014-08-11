f47a2a4f3e7f803f2f692b46b7db277176451d0d
Jiri Helmich <jiri@helmich.cz>
2014-08-11 20:56:04 +0200

    non-minified libs used

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -15,16 +15,16 @@
         },
         paths: {
             "lib": "lib",
-            "angular": "/webjars/angularjs/1.2.18/angular.min",
+            "angular": "/webjars/angularjs/1.2.18/angular",
             "highcharts": "/webjars/highcharts/4.0.3/highcharts-all",
             "domReady": "/webjars/requirejs-domready/2.0.1/domReady",
-            "angular-resource": "/webjars/angularjs/1.2.18/angular-resource.min",
-            "angular-route": "/webjars/angularjs/1.2.18/angular-route.min",
-            "angular-ui": "/webjars/angular-ui/0.4.0/angular-ui.min",
-            "highcharts-ng": "/webjars/highcharts-ng/0.0.6/highcharts-ng.min",
-            "ui.bootstrap": "/webjars/angular-ui-bootstrap/0.11.0/ui-bootstrap.min",
-            "angular-loading-bar": "/webjars/angular-loading-bar/0.4.3/loading-bar.min",
-            "underscore": "/webjars/underscorejs/1.6.0/underscore-min"
+            "angular-resource": "/webjars/angularjs/1.2.18/angular-resource",
+            "angular-route": "/webjars/angularjs/1.2.18/angular-route",
+            "angular-ui": "/webjars/angular-ui/0.4.0/angular-ui",
+            "highcharts-ng": "/webjars/highcharts-ng/0.0.6/highcharts-ng",
+            "ui.bootstrap": "/webjars/angular-ui-bootstrap/0.11.0/ui-bootstrap",
+            "angular-loading-bar": "/webjars/angular-loading-bar/0.4.3/loading-bar",
+            "underscore": "/webjars/underscorejs/1.6.0/underscore"
         }
     });
 
