ebfa1a0ae8107a2067d6e4e89f4dbdaa52e2887f
Jiří Helmich <jiri@helmich.cz>
2014-10-24 10:14:21 +0200

    template rename - DI

--- src/app/WebModule.scala
+++ src/app/WebModule.scala
@@ -5,8 +5,9 @@ class WebModule extends Module {
   binding to new ApplicationController
   binding to new DataCubeController
   binding to new MapController
+  binding to new VisualizationController
+  binding to new VisualizerController
   binding to new api.DataCubeApiController
   binding to new api.MapApiController
   binding to new api.VisualizationApiController
-  binding to new VisualizationController
 }
