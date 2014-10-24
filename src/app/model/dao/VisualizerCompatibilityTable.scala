fa62029490a4e0e7427452317f54809ae40fc27f
Jiří Helmich <jiri@helmich.cz>
2014-10-24 08:43:38 +0200

    changed DB structure to support compatibility checks

--- src/app/model/dao/VisualizerCompatibilityTable.scala
+++ src/app/model/dao/VisualizerCompatibilityTable.scala
@@ -4,15 +4,18 @@ import org.joda.time.DateTime
 import play.api.db.slick.Config.driver.simple._
 import PortableJodaSupport._
 
-case class VisualizerCompatibility(id: Long, dataSourceId: Long, visualizationId: Long, var createdUtc: Option[DateTime] = None, var modifiedUtc: Option[DateTime] = None) extends IdentifiedEntity
+case class VisualizerCompatibility(id: Long, visualizerId: Long, dataSourceId: Long, visualizationId: Long, var createdUtc: Option[DateTime] = None, var modifiedUtc: Option[DateTime] = None) extends IdentifiedEntity
 
 class VisualizerCompatibilityTable(tag: Tag) extends Table[VisualizerCompatibility](tag, "VISUALIZERS_COMPATIBILITY") with IdentifiedEntityTable[VisualizerCompatibility] {
   val dataSources = TableQuery[DataSourcesTable]
   val visualizations = TableQuery[VisualizationTable]
+  val visualizers = TableQuery[VisualizerTable]
 
   def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
 
-  def visualization = foreignKey("VC_VISUALIZER_FK", visualizationId, visualizations)(_.id)
+  def visualization = foreignKey("VC_VISUALIZATION_FK", visualizationId, visualizations)(_.id)
+
+  def visualizer = foreignKey("VC_VISUALIZER_FK", visualizerId, visualizers)(_.id)
 
   def dataSource = foreignKey("VC_DATA_SOURCE_FK", dataSourceId, dataSources)(_.id)
 
@@ -24,7 +27,9 @@ class VisualizerCompatibilityTable(tag: Tag) extends Table[VisualizerCompatibili
 
   def visualizationId = column[Long]("VISUALIZATION_ID")
 
-  def * = (id, visualizationId, dataSourceId, createdUtc, modifiedUtc) <>(VisualizerCompatibility.tupled, VisualizerCompatibility.unapply _)
+  def visualizerId = column[Long]("VISUALIZER_ID")
+
+  def * = (id, visualizerId, dataSourceId, visualizationId, createdUtc, modifiedUtc) <>(VisualizerCompatibility.tupled, VisualizerCompatibility.unapply _)
 
 }
 
