24dac54843b3d4eb2400db95ae18a06949b2cdbc
Jiří Helmich <jiri@helmich.cz>
2015-03-13 11:49:20 +0100

    part of #20

--- src/app/controllers/VisualizationController.scala
+++ src/app/controllers/VisualizationController.scala
@@ -1,9 +1,10 @@
 package controllers
 
 import model.entity.{PipelineEvaluation, PipelineEvaluationId}
+import model.rdf.Graph
 import model.service.PipelineService
 import play.api.db.slick._
-import play.api.mvc.{Result, Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 import scaldi.{Injectable, Injector}
 import views.VisualizerRoute
 
@@ -11,13 +12,33 @@ class VisualizationController(implicit inj: Injector) extends Controller with In
 
   val pipelineService = inject[PipelineService]
 
+  def ttlds = DBAction { implicit rws =>
+    Ok(views.html.visualization.ttlds())
+  }
+
+  def ttlupload = Action(parse.multipartFormData) { request =>
+    request.body.file("ttlfile").map { ttl =>
+      val filename = ttl.filename
+      val contentType = ttl.contentType
+      Graph(ttl.ref.file).map { g =>
+        val urn = g.pushToRandomGraph
+        Redirect(routes.VisualizationController.discover(Some("http://live.payola.cz:8890/sparql"), Some(urn)))
+      }.getOrElse {
+        UnprocessableEntity
+      }
+    }.getOrElse {
+      Redirect(routes.ApplicationController.index()).flashing(
+        "error" -> "Missing file")
+    }
+  }
+
   def visualize(pipelineEvaluationId: Long) = DBAction { implicit rws =>
 
     withEvaluation(pipelineEvaluationId) { e =>
 
       val maybeVisualizerTemplateUri = e.pipeline.componentInstances.find(!_.hasOutput).map(_.componentTemplate.uri)
 
-      maybeVisualizerTemplateUri.map {uri =>
+      maybeVisualizerTemplateUri.map { uri =>
         TemporaryRedirect(VisualizerRoute.route(uri, e.id.get))
       }.getOrElse {
         NotFound
@@ -27,18 +48,18 @@ class VisualizationController(implicit inj: Injector) extends Controller with In
   }
 
   def treemap(pipelineEvaluationId: Long) = DBAction { implicit rws =>
-       withEvaluation(pipelineEvaluationId) { e =>
-         Ok(views.html.visualizer.treemap(e.id.get))
-       }
+    withEvaluation(pipelineEvaluationId) { e =>
+      Ok(views.html.visualizer.treemap(e.id.get))
+    }
   }
 
-  private def withEvaluation(id: Long)(func: PipelineEvaluation => Result)(implicit session: Session) : Result = {
+  private def withEvaluation(id: Long)(func: PipelineEvaluation => Result)(implicit session: Session): Result = {
     pipelineService.findEvaluationById(PipelineEvaluationId(id)).map(func).getOrElse(NotFound)
   }
 
   def discover(endpointUrl: Option[String] = None, graphUris: Option[String] = None) = Action {
 
-    TemporaryRedirect("/pipelines#/discover?endpointUrl="+endpointUrl.orNull+graphUris.map("&graphUris="+_).getOrElse(""))
+    TemporaryRedirect("/pipelines#/discover?endpointUrl=" + endpointUrl.orNull + graphUris.map("&graphUris=" + _).getOrElse(""))
   }
 
 }
--- src/app/model/rdf/Graph.scala
+++ src/app/model/rdf/Graph.scala
@@ -1,9 +1,17 @@
 package model.rdf
 
-import java.io.{StringWriter, File, FileInputStream, StringReader}
+import java.io._
 import java.net.URL
+import java.util.UUID
 
-import com.hp.hpl.jena.rdf.model.ModelFactory
+import akka.actor.Props
+import com.hp.hpl.jena.rdf.model.{Model, ModelFactory}
+import model.rdf.Graph
+import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.DefaultHttpClient
+import play.api.libs.concurrent.Akka
 
 case class Graph(jenaModel: com.hp.hpl.jena.rdf.model.Model) {
 
@@ -32,6 +40,35 @@ case class Graph(jenaModel: com.hp.hpl.jena.rdf.model.Model) {
     val sw = new StringWriter()
     jenaModel.write(sw, "N3", null)
     sw.toString
+  }
+
+  def pushToRandomGraph : String = {
+
+    val endpoint: String = "http://live.payola.cz:8890"
+    val graphUri: String = "urn:"+UUID.randomUUID().toString
+
+    val requestUri = String.format("%s/sparql-graph-crud-auth?graph-uri=%s", endpoint, graphUri)
+
+    val stringData = toTTL
+
+    val credentials = new UsernamePasswordCredentials("dba", "dba")
+    val httpClient = new DefaultHttpClient()
+    val post = new HttpPost(requestUri)
+    post.addHeader("X-Requested-Auth", "Digest")
+    try {
+      httpClient.getCredentialsProvider.setCredentials(AuthScope.ANY, credentials)
+      val stringEntity = new StringEntity(stringData, "UTF-8")
+      post.setEntity(stringEntity)
+      httpClient.execute(post)
+
+      graphUri
+    }
+    catch {
+      case e: Throwable => throw e
+    }
+    finally {
+      httpClient.getConnectionManager.shutdown()
+    }
   }
 
 }
