03823378df3e84dd47aeb117ad1ae2ee03e94fe4
Jiří Helmich <jiri@helmich.cz>
2015-04-09 11:32:18 +0200

    Fixes #12

--- src/app/controllers/MapController.scala
+++ src/app/controllers/MapController.scala
@@ -8,5 +8,8 @@ class MapController(implicit inj: Injector) extends Controller with Injectable {
   def visualize = Action {
     Ok(views.html.visualizer.map(""))
   }
+  def visualizeOL = Action {
+    Ok(views.html.visualizer.map(""))
+  }
 
 }
