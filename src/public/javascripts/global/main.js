7fb32126e6c8295b4f7b2ab4806836e3950ede92
Jiří Helmich <jiri@helmich.cz>
2015-05-18 09:50:39 +0200

    uploading text on button

--- src/public/javascripts/global/main.js
+++ src/public/javascripts/global/main.js
@@ -2,9 +2,9 @@ require(['material', 'jquery'], function (material, $) {
     material.init();
 
 
-    $("#use").click(function(){
+    $("#use").parent(.click(function(){
         $("#use").attr("disabled", "disabled");
         $("#use").html("Uploading...");
-        return true;
+        $("#use").parents("form").submit();
     });
 });
