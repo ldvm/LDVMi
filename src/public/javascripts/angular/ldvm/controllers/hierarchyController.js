bf7bf4ec4e74dfacf675b788ba2d651a4e09c303
Jiří Helmich <jiri@helmich.cz>
2015-10-18 20:59:42 +0200

    Fixes #61

--- src/public/javascripts/angular/ldvm/controllers/hierarchyController.js
+++ src/public/javascripts/angular/ldvm/controllers/hierarchyController.js
@@ -7,48 +7,89 @@ define(['angular', './controllers'], function (ng) {
             '$scope',
             '$routeParams',
             'Visualization',
-            function (
-                $scope,
-                $routeParams,
-                visualization
-            ) {
+            function ($scope,
+                      $routeParams,
+                      visualization) {
 
                 var id = $scope.id = $routeParams.id;
-                if(!id){
+                if (!id) {
                     return;
                 }
 
                 $scope.visType = $routeParams.type;
 
-                $scope.schemes = [];
+                $scope.schemes = [];
                 $scope.scheme = null;
                 $scope.queryingDataset = null;
 
                 var promise = visualization.skos.schemes(id);
                 $scope.queryingDataset = true;
-                promise.then(function(schemes){
+                promise.then(function (schemes) {
                     $scope.schemes = schemes;
                     $scope.queryingDataset = false;
                 });
 
-                $scope.uri = function(scheme){
+                $scope.uri = function (scheme) {
                     return "#/hierarchy/" + $scope.visType + "/" + $scope.id + "/?uri=" + encodeURIComponent(scheme.uri);
                 };
 
-                $scope.embedUrl = function(visType){
-                    return "/visualize/embed/" + visType + "/" + $scope.id + "?schemeUri=" + encodeURIComponent($routeParams.uri);
+                $scope.embedUrl = function (visType, includeHost) {
+                    return (includeHost ? "http://" + document.location.host : "") + "/visualize/embed/" + visType + "/" + $scope.id + "?schemeUri=" + encodeURIComponent($routeParams.uri);
                 };
 
-                $scope.loadScheme = function(schemeUri){
+                $scope.loadScheme = function (schemeUri) {
                     $scope.queryingDataset = true;
                     var schemePromise = visualization.skos.scheme(id, schemeUri);
-                    schemePromise.then(function(scheme){
+                    schemePromise.then(function (scheme) {
                         $scope.queryingDataset = false;
                         $scope.scheme = scheme;
                     });
                 };
 
-                if($routeParams.uri){
+                $scope.switch = function (type) {
+                    $scope.visType = type;
+                };
+
+                $scope.visualizations = [
+                    {
+                        key: 'sunburst',
+                        label: 'Sunburst'
+                    },
+                    {
+                        key: 'treemap',
+                        label: 'Treemap'
+                    },
+                    {
+                        key: 'bilevel',
+                        label: 'BiLevel'
+                    },
+                    {
+                        key: 'packLayout',
+                        label: 'Pack Layout'
+                    },
+                    {
+                        key: 'force',
+                        label: 'Force Layout'
+                    },
+                    {
+                        key: 'cluster',
+                        label: 'Rotating Cluster'
+                    },
+                    {
+                        key: 'partition',
+                        label: 'Partition Layout'
+                    },
+                    {
+                        key: 'tree',
+                        label: 'Tree'
+                    },
+                    {
+                        key: 'radialTree',
+                        label: 'Radial tree'
+                    }
+                ];
+
+                if ($routeParams.uri) {
                     $scope.loadScheme($routeParams.uri);
                 }
 
