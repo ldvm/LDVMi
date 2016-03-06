2d881753ed5f0068e992ed79606b408265e7c053
Jiří Helmich <jiri@helmich.cz>
2016-03-06 15:53:47 +0100

    Fixes linkedpipes/visualizations#7

--- src/public/javascripts/angular/ldvm/directives/createDatasource.js
+++ src/public/javascripts/angular/ldvm/directives/createDatasource.js
@@ -1,4 +1,4 @@
-define(['angular', './directives'], function (ng) {
+define(['angular', 'jquery', './directives'], function (ng, $) {
     'use strict';
 
     return ng.module('ldvm.directives')
@@ -31,6 +31,48 @@ define(['angular', './directives'], function (ng) {
                         return !$scope.maxSourcesCount || $scope.maxSourcesCount > $scope.sources.length;
                     }
 
+                },
+                link: function (scope, elm) {
+                    elm = $(elm);
+                    elm.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                        })
+                        .on('dragover dragenter', function () {
+                            elm.addClass('is-dragover');
+                        })
+                        .on('dragleave dragend drop', function () {
+                            elm.removeClass('is-dragover');
+                        })
+                        .on('drop', function (e) {
+                            var source = {type: 'file', files: []};
+                            var files = e.originalEvent.dataTransfer.files;
+                            var total = files.length;
+                            for (var i in files) {
+                                if (i !== "length" && i !== "item") {
+                                    var file = files[i];
+                                    var reader = new FileReader();
+                                    reader.onload = function (loadEvent) {
+                                        source.files.push({
+                                            lastModified: file.lastModified,
+                                            lastModifiedDate: file.lastModifiedDate,
+                                            name: file.name,
+                                            size: file.size,
+                                            type: file.type,
+                                            data: loadEvent.target.result
+                                        });
+                                        --total;
+
+                                        if(total < 1){
+                                            scope.$apply(function(){
+                                                scope.sources.push(source);
+                                            });
+                                        }
+                                    };
+                                    reader.readAsDataURL(file);
+                                }
+                            }
+                        });
                 },
                 templateUrl: '/assets/javascripts/angular/ldvm/partials/createDatasource.html'
             };
