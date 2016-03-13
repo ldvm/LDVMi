9b41c98205a8425f021fd2fcb45cbc42777e21b3
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-03-13 16:17:55 +0100

    [MapsVisualizer] Added inset shadow to the map

--- src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/components/ConfiguratorLayout.js
+++ src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/components/ConfiguratorLayout.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { Row, Col } from 'react-flexbox-grid'
+import Paper from 'material-ui/lib/paper';
 import { Application } from '../../../manageApp/models'
 import Sidebar from '../containers/Sidebar'
 import FillInScreen from '../../../../misc/components/FillInScreen'
@@ -22,8 +23,30 @@ const mapStyle = {
   zIndex: 2
 };
 
+const insetShadowWrapperStyle = {
+  width: '100%',
+  height: '10px',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: 3,
+  overflow: 'hidden'
+};
+
+const insetShadowStyle = {
+  width: '100%',
+  height: '20px',
+  position: 'relative',
+  top: '-20px',
+  left: 0,
+  zIndex: 3
+};
+
 const ConfiguratorLayout = ({ application }) => {
   return <div style={wrapperStyle}>
+    <div style={insetShadowWrapperStyle}>
+      <Paper style={insetShadowStyle} />
+    </div>
     <div style={sidebarStyle}>
       <BodyPadding>
         <Sidebar application={application} />
