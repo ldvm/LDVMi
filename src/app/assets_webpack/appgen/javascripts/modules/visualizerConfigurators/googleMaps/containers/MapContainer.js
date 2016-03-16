791371adbe92fe7490eb490435e2f5b5776f61de
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-03-16 12:17:30 +0100

    [MapsVisualizer] Added marker clusterer to map

--- src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/containers/MapContainer.js
+++ src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/containers/MapContainer.js
@@ -2,18 +2,25 @@ import React from 'react'
 import { createSelector } from 'reselect'
 import { connect } from 'react-redux'
 import { Marker } from 'react-google-maps'
+import MarkerClusterer from "react-google-maps/lib/addons/MarkerClusterer";
 import GoogleMap from '../../../../misc/components/GoogleMap'
 import { markersSelector } from '../ducks/markers'
 
 const MapContainer = ({ markers }) => {
   return <GoogleMap>
-    {markers.map((marker, index) =>
-      <Marker
-        key={marker.uri}
-        position={marker.coordinates}
-        defaultAnimation={null}
-      />
-    )}
+    <MarkerClusterer
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {markers.map((marker, index) =>
+        <Marker
+          key={marker.uri}
+          position={marker.coordinates}
+          defaultAnimation={null}
+        />
+      )}
+    </MarkerClusterer>
   </GoogleMap>;
 };
 
