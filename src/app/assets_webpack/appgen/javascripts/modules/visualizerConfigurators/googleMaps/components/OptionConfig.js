8b3f538b4ac4cc4f67de4d1db74ea193130435dd
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-03-16 10:48:50 +0100

    [MapsVisualizer] Option configs now contain nice indication icon

--- src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/components/OptionConfig.js
+++ src/app/assets_webpack/appgen/javascripts/modules/visualizerConfigurators/googleMaps/components/OptionConfig.js
@@ -4,20 +4,32 @@ import IconMenu from 'material-ui/lib/menus/icon-menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import IconButton from 'material-ui/lib/icon-button';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import FontIcon from 'material-ui/lib/font-icon'
 import { Option } from '../models'
 import Padding from '../../../../misc/components/Padding'
 import makePureRender from '../../../../misc/makePureRender'
+import MaterialTheme from '../../../../misc/materialTheme';
 import { optionModes as modes } from  '../models'
 
 const iconMenuStyle = {
   float: 'right',
   marginTop: '-12px',
-  marginRight: '-12px'
+  marginRight: '-12px',
+  position: 'relative',
+  zIndex: 1
 };
 
 const labelStyle = {
   fontSize: '0.9rem',
-  color: 'rgba(0, 0, 0, 0.8)'
+  color: 'rgba(0, 0, 0, 0.8)',
+  position: 'relative',
+  zIndex: 0,
+  paddingLeft: MaterialTheme.spacing.desktopGutterMini * 5 + 'px'
+};
+
+const iconStyle = {
+  position: 'absolute',
+  left: 0
 };
 
 const countStyle = {
@@ -30,17 +42,30 @@ const dividerStyle = {
   clear: 'both'
 };
 
+const colors = {
+  [modes.USER_DEFINED]: 'inherited',
+  [modes.SELECT_ALWAYS]: 'rgba(0, 0, 0, 0.3)',
+  [modes.SELECT_NEVER]: 'rgba(0, 0, 0, 0.3)'
+};
+
+const icons = {
+  [modes.USER_DEFINED]: 'chevron_right',
+  [modes.SELECT_ALWAYS]: 'check',
+  [modes.SELECT_NEVER]: 'close'
+};
+
 const configStyles = {
   [modes.USER_DEFINED]: {
   },
   [modes.SELECT_ALWAYS]: {
-    color: 'rgba(0, 0, 0, 0.3)'
+    color: colors[modes.SELECT_ALWAYS]
   },
   [modes.SELECT_NEVER]: {
-    color: 'rgba(0, 0, 0, 0.3)',
+    color: colors[modes.SELECT_NEVER],
     textDecoration: 'line-through'
   }
 };
+
 
 const OptionConfig = ({ option, configureOption }) => {
   const { count, mode, skosConcept } = option;
@@ -69,6 +94,7 @@ const OptionConfig = ({ option, configureOption }) => {
         />
       </IconMenu>
       <div style={Object.assign({}, labelStyle, configStyles[mode])}>
+        <FontIcon className="material-icons" style={iconStyle} color={colors[mode]}>{icons[mode]}</FontIcon>
         {skosConcept.label}{' '}
         {count !== null && <span style={countStyle}>({count})</span>}
       </div>
