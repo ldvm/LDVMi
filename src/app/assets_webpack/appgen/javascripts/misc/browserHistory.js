b46911d30a1bbb77fdb2747371ebed9b6f94a83e
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-03-22 20:30:39 +0100

    [Core] Switched to dynamic baseUrl provided by the view

--- src/app/assets_webpack/appgen/javascripts/misc/browserHistory.js
+++ src/app/assets_webpack/appgen/javascripts/misc/browserHistory.js
@@ -1,9 +1,8 @@
 import { useRouterHistory } from 'react-router'
 import { createHistory, useBasename } from 'history'
-import { baseUrl } from '../config'
 
 // Custom history used for react-router. Defines baseURL so
 // that the app can run in a subdirectory.
 export default  useRouterHistory(useBasename(createHistory))({
-  basename: baseUrl
+  basename: window.appgen.baseUrl
 });
