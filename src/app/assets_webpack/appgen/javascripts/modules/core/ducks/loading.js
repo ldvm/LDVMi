3d8015ea2f2921d166e7554c52dd51c767ab839a
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-03-22 23:08:29 +0100

    [Core] Fixed loading reducer

--- src/app/assets_webpack/appgen/javascripts/modules/core/ducks/loading.js
+++ src/app/assets_webpack/appgen/javascripts/modules/core/ducks/loading.js
@@ -2,18 +2,31 @@ import { actionTypes as formActions } from 'redux-form'
 import { createSelector } from 'reselect'
 import moduleSelector from '../selector'
 
-// Actions
+const START = 'START';
+const SUCCESS = 'SUCCESS';
+const ERROR = 'ERROR';
+
+// Reducer
+
 /** Maintains the number of running background tasks (i. e. AJAX calls) */
 export default function loadingReducer(state = 0, action) {
+  if (action.type.match(START + '$')) {
+    return state + 1;
+  }
+
+  if (action.type.match(SUCCESS + '$') || action.type.match(ERROR + '$')) {
+    return state - 1;
+  }
+
   switch (action.type) {
     case formActions.START_SUBMIT:
       return state + 1;
     case formActions.STOP_SUBMIT:
     case formActions.SUBMIT_FAILED:
       return state - 1;
-    default:
-      return 0;
   }
+
+  return state;
 }
 
 // Selectors
