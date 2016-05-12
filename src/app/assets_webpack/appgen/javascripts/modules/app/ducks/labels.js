cce7a084b5e8270e2b95c202910caa2bfaf3243a
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-05-12 22:21:49 +0200

    [Labels] Throttling the getLabels request

--- src/app/assets_webpack/appgen/javascripts/modules/app/ducks/labels.js
+++ src/app/assets_webpack/appgen/javascripts/modules/app/ducks/labels.js
@@ -10,10 +10,27 @@ export const GET_LABELS_START = prefix('GET_LABELS_START');
 export const GET_LABELS_ERROR = prefix('GET_LABELS_ERROR');
 export const GET_LABELS_SUCCESS = prefix('GET_LABELS_SUCCESS');
 
+let resourceUrisBuffer = [];
+
 export function getLabels(resourceUris) {
-  return withApplicationId(id => {
-    const promise = api.getLabels(id, resourceUris);
-    return createAction(GET_LABELS, { promise });
+  return withApplicationId(id => dispatch => {
+    // To avoid fetching the labels one by one, we push the incoming resource URIs into a buffer and
+    // wait a little bit before making the request. We basically 'throttle' the API request. Once
+    // the wait is over, we make a request to get labels for all resources in the buffer. This deals
+    // with the typical situation when React renders many Label components at once and all of them
+    // invoke this action.
+    //
+    // This solution is not exactly clean as it uses a static buffer expecting all labels
+    // to belong to the same application (data source). Right now it's not an issue.
+
+    resourceUrisBuffer = resourceUrisBuffer.concat(resourceUris);
+    setTimeout(() => {
+      if (resourceUrisBuffer.length > 0) {
+        const promise = api.getLabels(id, resourceUrisBuffer);
+        dispatch(createAction(GET_LABELS, { promise }));
+        resourceUrisBuffer = [];
+      }
+    }, 200);
   })
 }
 
