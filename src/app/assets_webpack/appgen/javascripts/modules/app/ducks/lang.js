61091886f87648f6bcc272df3c6bde329899b8c2
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-05-13 02:49:54 +0200

    [Labels] Extracting available languages from the Redux traffic

--- src/app/assets_webpack/appgen/javascripts/modules/app/ducks/lang.js
+++ src/app/assets_webpack/appgen/javascripts/modules/app/ducks/lang.js
@@ -1,22 +1,18 @@
-import { Map, fromJS } from 'immutable'
+import { Set } from 'immutable'
 import { combineReducers } from 'redux'
 import { createSelector } from 'reselect'
+import { GET_APPLICATION_START } from './application'
 import moduleSelector from '../selector'
 import createAction from '../../../misc/createAction'
 import prefix from '../prefix'
+import { extractLanguages } from '../misc/languageUtils'
 
 // Actions
 
 export const CHANGE_LANGUAGE = prefix('CHANGE_LANGUAGE');
-export const SET_AVAILABLE_LANGUAGES = prefix('SET_AVAILABLE_LANGUAGE');
 
 export function changeLanguage(lang) {
   return createAction(CHANGE_LANGUAGE, { lang });
-}
-
-// TODO: where to get those?
-export function setAvailableLanguages(availableLanguages) {
-  return createAction(SET_AVAILABLE_LANGUAGES, { availableLanguages });
 }
 
 // Reducers
@@ -28,12 +24,22 @@ const currentReducer = (state = 'cs', action) => {
   return state;
 };
 
-const availableReducer = (state = Map({ 'cs': 'Česky', 'en': 'English' }), action) => {
-  if (action.type == SET_AVAILABLE_LANGUAGES) {
-    return fromJS(action.payload.availableLanguages);
+const availableReducer = (state = new Set(), action) => {
+  switch (action.type) {
+    case GET_APPLICATION_START:
+      return new Set();
   }
 
-  return state;
+  // This is were the power of Flux/Redux truly shines. We need to get somehow the list of
+  // available languages. The list is not available anywhere, we need to find out by ourselves.
+  // One of the main aspects of Flux/Redux is that every action is dispatched to every
+  // store/reducer. So we can just sit here and monitor all traffic going through the application,
+  // regardless of where it comes from (it might come from server, it might be user generated,
+  // who knows). And since we have access to the traffic, we just parse it and look for "patterns"
+  // that suggest available languages (typically an object with a "variants" property).
+  // (Okay, this solution is still pretty dirty, but it's the only that actually works and
+  // thanks to Redux it's at least nicely implemented).
+  return state.union(extractLanguages(action.payload));
 };
 
 export default combineReducers({
--- src/app/assets_webpack/appgen/javascripts/modules/app/misc/languageUtils.js
+++ src/app/assets_webpack/appgen/javascripts/modules/app/misc/languageUtils.js
@@ -1,7 +1,24 @@
-import { Map, fromJS } from 'immutable'
+import { Map, fromJS, Set, Collection } from 'immutable'
+import languages from '../../../misc/languages'
 
 export const NOLANG = 'nolang';
 
+// There is a little confusion with what the "localized value" is. In the broadest sense it means
+// a value that should be displayed with...
+// TODO: fix this naming craziness... rename extractLocalizedValue into something better?
+
+/**
+ * Extracts localized value in given language. If value for given language is not available, the
+ * function returns any value that is available. If there is no value available at all, defaultValue
+ * is returned.
+ *
+ * @param {string} lang code of the required language
+ * @param {*} value can be anything, the function will parse it and if it recognizes familiar
+ *  format (object or Immutable.Map with "variants" property) it will attempt to extract
+ *  value in required language.
+ * @param {string} defaultValue function will fallback to this value if no other value is available
+ * @returns {string} localized value
+ */
 export function extractLocalizedValue(lang, value, defaultValue = null) {
 
   if (value === undefined || value === null) {
@@ -35,6 +52,13 @@ export function extractLocalizedValue(lang, value, defaultValue = null) {
   }
 }
 
+/**
+ * Returns localized value in standardized format. The standardized format means an instance
+ * of Immutable.Map with the "variants" property. The function tries its best to convert the
+ * input value into the standardized format.
+ * @param {*} value in any format
+ * @returns {Map} normalized value
+ */
 export function normalizeLocalizedValue(value) {
   const normalizedEmpty = fromJS({ variants: {} });
 
@@ -51,6 +75,42 @@ export function normalizeLocalizedValue(value) {
   }
 }
 
+/**
+ * Returns if there is any value to be displayed
+ * @param {*} value
+ * @return {boolean}
+ */
 export function isLocalizedValueEmpty(value) {
   return normalizeLocalizedValue(value).get('variants').size === 0;
+}
+
+/**
+ * Recursively searches through the value and extracts available languages. It's looking for
+ * objects or Immutable.Maps with the "variants" property. It returns a Set of all discovered
+ * valid languages.
+ * @param {*} value
+ * @return {Set}
+ */
+export function extractLanguages(value) {
+  if (value === undefined || value === null) {
+    return new Set();
+  } else if (value instanceof Collection.Keyed) {
+    if (value.has('variants')) {
+      return new Set(value.get('variants').keySeq().filter(lang => lang in languages));
+    } else {
+      return value.valueSeq().reduce((langs, v) => langs.merge(extractLanguages(v)), new Set());
+    }
+  } else if (value instanceof Collection.Indexed) {
+    return value.reduce((langs, val) => langs.merge(extractLanguages(val)), new Set());
+  } else if (typeof value === 'object' && !Array.isArray(value)) {
+    if ('variants' in value) {
+      return new Set(Object.keys(value.variants).filter(lang => lang in languages));
+    } else {
+      return Object.keys(value).reduce((langs, k) => langs.merge(extractLanguages(value[k])), new Set());
+    }
+  } else if (Array.isArray(value)) {
+    return value.reduce((langs, v) => langs.merge(extractLanguages(v)), new Set());
+  } else {
+    return new Set();
+  }
 }
