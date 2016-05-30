08ed7617a93b880220ade1c9728d6fdd8f50e0fe
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-05-30 18:30:51 +0200

    [Core] Nicer loading indicator

--- src/app/assets_webpack/appgen/javascripts/components/Loading.js
+++ src/app/assets_webpack/appgen/javascripts/components/Loading.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import CircularProgress from 'material-ui/lib/circular-progress';
+import RefreshIndicator from 'material-ui/lib/refresh-indicator';
 import MaterialTheme from '../misc/materialTheme';
+import * as theme from '../misc/theme'
 
 const Loading = (props) => {
   const spacing = MaterialTheme.spacing.desktopGutterLess + 'px';
 
-  const styles = {
+  const containerStyle = {
     margin: spacing + ' 0',
+    position: 'relative',
+    lineHeight: '50px'
   };
 
   const circleStyles = {
     display: 'inline-block',
-    marginRight: '8px'
+    position: 'relative'
   };
 
   const labelStyles = {
     display: 'inline-block',
     position: 'relative',
-    top: '-20px'
+    left: '10px',
+    top: '-10px',
+    color: '#8a8a8a'
   };
 
-  return <div style={styles}>
-    <CircularProgress size={0.5} style={circleStyles} />
+  return <div style={containerStyle}>
+    <RefreshIndicator
+      size={40}
+      left={0}
+      top={0}
+      loadingColor={theme.warning}
+      status="loading"
+      style={circleStyles}
+    />
     <div style={labelStyles}>{props.children}</div>
   </div>
 };
