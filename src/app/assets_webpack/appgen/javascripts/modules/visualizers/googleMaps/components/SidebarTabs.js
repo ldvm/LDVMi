6c8bd09b5d2cd5abffc1fa31b443156bcbff21b7
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-06-01 17:04:02 +0200

    Fixed tab switching in Google Map configurator
    The FillInScreen component has to be force-updated when a tab becomes active.

--- src/app/assets_webpack/appgen/javascripts/modules/visualizers/googleMaps/components/SidebarTabs.js
+++ src/app/assets_webpack/appgen/javascripts/modules/visualizers/googleMaps/components/SidebarTabs.js
@@ -1,55 +1,57 @@
-import React, { PropTypes } from 'react'
-import { List, Map } from 'immutable'
+import React, { Component, PropTypes } from 'react'
+import { Map } from 'immutable'
 import Tabs from 'material-ui/Tabs/Tabs';
 import Tab from 'material-ui/Tabs/Tab';
 import Paper from 'material-ui/Paper';
 import RefreshMapButton from '../containers/RefreshMapButton'
 import SaveButton from '../containers/SaveButton'
 import FillInScreen from '../../../../components/FillInScreen'
-import Button from '../../../../components/Button'
 import Padding from '../../../../components/Padding'
 import FilterConfig from '../containers/FilterConfig'
 import FilterPreview from '../containers/FilterPreview'
-import { Application } from '../../../app/models'
 
-const SidebarTabs = ({ filters }) => {
+class SidebarTabs extends Component {
+  static propTypes = {
+    filters: PropTypes.instanceOf(Map)
+  };
 
-  return <Paper zDepth={2}>
-    <Tabs inkBarStyle={{ backgroundColor: 'white' }}>
-      <Tab label="Configure">
-        <FillInScreen marginBottom={100}>
-          <div>
-            {filters.toList().map(filter =>
-              <div key={filter.property.uri}>
-                <FilterConfig filter={filter} />
-              </div>
-            )}
-          </div>
-        </FillInScreen>
-        <Padding space={2}>
-          <SaveButton fullWidth />
-        </Padding>
-      </Tab>
-      <Tab label="Preview">
-        <FillInScreen marginBottom={100}>
-          <div>
-            {filters.toList().map(filter =>
-              <div key={filter.property.uri}>
-                <FilterPreview filter={filter} />
-              </div>
-            )}
-          </div>
-        </FillInScreen>
-        <Padding space={2}>
-          <RefreshMapButton fullWidth />
-        </Padding>
-      </Tab>
-    </Tabs>
-  </Paper>
-};
+  render() {
+    const { filters } = this.props;
 
-SidebarTabs.propTypes = {
-  filters: PropTypes.instanceOf(Map)
-};
+    return <Paper zDepth={2}>
+      {/* We need to force the FillInScreen component to update when a tab becomes active. */}
+      <Tabs inkBarStyle={{ backgroundColor: 'white' }} onChange={::this.forceUpdate}>
+        <Tab label="Configure">
+          <FillInScreen marginBottom={100}>
+            <div>
+              {filters.toList().map(filter =>
+                <div key={filter.property.uri}>
+                  <FilterConfig filter={filter} />
+                </div>
+              )}
+            </div>
+          </FillInScreen>
+          <Padding space={2}>
+            <SaveButton fullWidth />
+          </Padding>
+        </Tab>
+        <Tab label="Preview">
+          <FillInScreen marginBottom={100}>
+            <div>
+              {filters.toList().map(filter =>
+                <div key={filter.property.uri}>
+                  <FilterPreview filter={filter} />
+                </div>
+              )}
+            </div>
+          </FillInScreen>
+          <Padding space={2}>
+            <RefreshMapButton fullWidth />
+          </Padding>
+        </Tab>
+      </Tabs>
+    </Paper>
+  };
+}
 
 export default SidebarTabs;
