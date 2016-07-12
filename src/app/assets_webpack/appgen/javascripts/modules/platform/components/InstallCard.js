a99b390504930a68e869eff5061f30a5576b1c01
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-07-12 23:46:18 +0200

    [Install] Improved UI descriptions

--- src/app/assets_webpack/appgen/javascripts/modules/platform/components/InstallCard.js
+++ src/app/assets_webpack/appgen/javascripts/modules/platform/components/InstallCard.js
@@ -8,13 +8,16 @@ import PromiseResult from '../../core/components/PromiseResult'
 
 const InstallCard = ({ install, installResults, installStatus }) => {
   return (
-    <PaperCard title="Install" subtitle="Import default LDVM components">
+    <PaperCard title="Install" subtitle="Import default LDVM components and data sets">
       <div>Your LinkedPipes Application Generator instance seems to be empty. No visualizers have
       been found.</div>
-      <p><strong>Do you wish to install a couple of default LDVM components and data sources so that
+      <p><strong>Do you wish to install a couple of default LDVM components and data sets so that
       you can immediately start generating applications?</strong></p>
-      <p>The LDVM components and data sources will be downloaded from public GitHub repositories.
-      You need to be online to use this install script.</p>
+      <p>The LDVM components and data sets will be downloaded from public GitHub repositories.
+      You need to be online to use this install script. The data sets will be imported
+      to the local Virtuoso triple store for the best performance.</p>
+
+      <p>The whole process might take a couple of minutes.</p>
 
       {installStatus.done ?
         <div>
@@ -23,6 +26,7 @@ const InstallCard = ({ install, installResults, installStatus }) => {
               <Alert key={i} info>{result.get('message')}</Alert> :
               <Alert key={i} danger>{result.get('message')}</Alert>
           )}
+          <Alert success>Installation successfully finished. Please refresh this page</Alert>
         </div>
         :
         <div>
