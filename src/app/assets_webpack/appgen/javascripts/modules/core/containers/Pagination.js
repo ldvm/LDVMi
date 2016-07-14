9d15f20b33fd9ed953347905cfce9fff8f0d7c57
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-07-14 17:52:28 +0200

    [Core] Pagination shows only a limited subset of pages

--- src/app/assets_webpack/appgen/javascripts/modules/core/containers/Pagination.js
+++ src/app/assets_webpack/appgen/javascripts/modules/core/containers/Pagination.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Range } from 'immutable'
+import { Range, List } from 'immutable'
 import { connect } from 'react-redux'
 import { Paginator } from '../models'
 import { changePage, createPaginatorSelector } from '../ducks/pagination'
@@ -11,6 +11,21 @@ const Pagination = ({ paginator, page, changePage }) => {
   }
 
   const totalPages = Math.floor((paginator.totalCount + paginator.pageSize - 1) / paginator.pageSize);
+  const firstPage = 1;
+  const lastPage = totalPages + 1;
+  const SPACE = -1;
+
+  // As there might be too many pages, we just render the current page and the immediate pages
+  // before and after it. We also add the first page and the last page.
+  const pages = Range(Math.max(firstPage, page - 3), Math.min(lastPage, page + 4))
+    .toSet()
+    .add(firstPage).add(lastPage)
+    .toList().sort()
+    .reduce((pages, page) =>
+      // If the there is a big step between consecutive pages (e.g. first page and then 5th or
+      // something), we add a space between them.
+      page > pages.last() + 1 ?
+        pages.push(SPACE).push(page) : pages.push(page), new List());
 
   return (
     <div>
@@ -18,11 +33,12 @@ const Pagination = ({ paginator, page, changePage }) => {
         <PaginatorPage onTouchTap={() => changePage(page - 1)} label="< Prev" />
       }
       {' '}
-      {Range(1, totalPages + 1).map(i =>
-        <PaginatorPage key={i}
-          primary={i == page}
-          onTouchTap={() => changePage(i)}
-          label={i} />
+      {pages.map(i =>
+        i == SPACE ? ' ' :
+          <PaginatorPage key={i}
+            primary={i == page}
+            onTouchTap={() => changePage(i)}
+            label={i} />
       )}
       {' '}
       {page < totalPages &&
