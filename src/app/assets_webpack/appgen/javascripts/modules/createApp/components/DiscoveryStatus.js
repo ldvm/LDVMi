fbfb1b31164fd64ac26b5a1cb3ccd5d7445ae432
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-07-25 16:02:29 +0200

    [CreateApp] DiscoveryStatus made properly pure

--- src/app/assets_webpack/appgen/javascripts/modules/createApp/components/DiscoveryStatus.js
+++ src/app/assets_webpack/appgen/javascripts/modules/createApp/components/DiscoveryStatus.js
@@ -1,34 +1,33 @@
 import React, { PropTypes, Component } from 'react'
-import { PureComponent } from 'react-pure-render'
 import { Discovery } from '../models'
 import PaperCard from '../../../components/PaperCard'
 import LinearProgress from '../../../components/LinearProgress'
 import Alert from '../../../components/Alert'
+import makePureRender from '../../../misc/makePureRender'
 
-class DiscoveryStatus extends PureComponent {
-  // Because of the animated <LinearProgress /> component, the number of re-renders should be
-  // kept to minimum to increase the smoothness. Smart selectors and pure render component will
-  // do the trick.
+// Because of the animated <LinearProgress /> component, the number of re-renders should be
+// kept to minimum to increase the smoothness. Smart selectors and pure render component will
+// do the trick.
 
-  static propTypes = {
-    discovery: PropTypes.instanceOf(Discovery).isRequired
-  };
+const DiscoveryStatus = ({ discovery }) => (
+  <PaperCard
+    title={"Discovery of " + discovery.name}
+    subtitle="Generating all possible visualizations of input data sources"
+  >
+    {discovery.isFinished ?
+      discovery.isSuccess ?
+        <Alert success>The discovery successfully finished</Alert> :
+        <Alert danger>The discovery failed. Please try again</Alert> :
+      <div>
+        The discovery is in progress. You may leave this page and come back later.
+        <LinearProgress />
+      </div>
+    }
+  </PaperCard>
+);
 
-  render() {
-    const {discovery} = this.props;
-    return <PaperCard title={"Discovery of " + discovery.name}
-      subtitle="Generating all possible visualizations of input data sources">
-      {discovery.isFinished ?
-        discovery.isSuccess ?
-          <Alert success>The discovery successfully finished</Alert> :
-          <Alert danger>The discovery failed. Please try again</Alert> :
-        <div>
-          The discovery is in progress. You may leave this page and come back later.
-          <LinearProgress />
-        </div>
-      }
-    </PaperCard>
-  }
-}
+DiscoveryStatus.propTypes = {
+  discovery: PropTypes.instanceOf(Discovery).isRequired
+};
 
-export default DiscoveryStatus;
+export default makePureRender(DiscoveryStatus);
