b186db423f31fd1abea5dcc81070630d94aac975
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-07-27 00:32:10 +0200

    Updated propTypes for app's Standalone components

--- src/app/assets_webpack/appgen/javascripts/modules/visualizers/googleMaps/pages/Standalone.js
+++ src/app/assets_webpack/appgen/javascripts/modules/visualizers/googleMaps/pages/Standalone.js
@@ -10,8 +10,11 @@ const Standalone = ({ application, visualizer }) => <div>
   </div>;
 
 Standalone.propTypes = {
-  application: PropTypes.instanceOf(ApplicationModel).isRequired,
-  visualizer: PropTypes.instanceOf(Visualizer).isRequired
+  // As the component is at first initialized by the router, these props might not be available
+  // (that's why they are not required). But once the component is actually mounted, both props
+  // are properly there (injected from the Loader).
+  application: PropTypes.instanceOf(ApplicationModel),
+  visualizer: PropTypes.instanceOf(Visualizer)
 };
 
 export default Standalone;
