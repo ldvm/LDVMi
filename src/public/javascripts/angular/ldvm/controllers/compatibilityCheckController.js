2307f7cb9748be1f311cf3c997d9a6d3a5b6585c
Jiří Helmich <jiri@helmich.cz>
2017-04-03 22:46:50 +0200

    Update compatibilityCheckController.js

--- src/public/javascripts/angular/ldvm/controllers/compatibilityCheckController.js
+++ src/public/javascripts/angular/ldvm/controllers/compatibilityCheckController.js
@@ -11,7 +11,7 @@ define(['angular', './controllers'], function (ng) {
 
                 var pipelineId = $routeParams.id;
                 var l = window.location;
-                var isSsl = location.protocol === 'https:';
+                var isSsl = l.protocol === 'https:';
                 var wsProtocol = isSsl ? "wss" : "ws";
                 var url = wsProtocol + "://" + l.host + "/api/v1/compatibility/check/" + pipelineId;
 
