996ea4a0a32b97b8ed87341af2ab600eeafd2668
vvancak <vladislav.vancak@gmail.com>
2017-05-23 00:55:26 +0200

    TimeLine: Visualizer API creating

--- src/app/model/assistant/rest/EventPeopleRequest.scala
+++
@@ -1,9 +0,0 @@
-package model.assistant.rest
-
-import play.api.libs.json.Json
-
-object EventPeopleRequest {
-  case class EventPeopleRequest(event: String)
-  implicit val writes = Json.writes[EventPeopleRequest]
-  implicit val reads = Json.reads[EventPeopleRequest]
-}
